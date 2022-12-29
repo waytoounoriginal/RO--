ac7c63ee7ed82ffab57ae9b19be4ee7d855d5487
Mihai4544 <mihai.tira@yahoo.ro>
2022-12-29 20:01:53 +0200

    first commit

---
+++ ro.h
@@ -0,0 +1,130 @@
+#include <iostream>
+
+//  C++ keywords
+#define spațiu namespace
+#define utilizând using
+#define șablon template
+#define tip typename
+#define definește_tip typedef
+
+#define clasă class
+#define structură struct
+#define uniune union
+
+#define adevărat true
+#define fals false
+#define nu !
+#define _nu not
+
+#define lung long
+#define nesemnat unsigned
+#define semnat signed
+
+#define nul NULL
+#define _nul_ nullptr
+#define întoarce return
+
+#define privat private
+
+#define nou new
+#define șterge delete
+
+#define oprește break
+#define continuă continue
+
+#define acesta this
+
+#define cât_timp while
+#define pentru for
+#define fă do
+#define în_funcție switch
+#define cazul case
+
+#define dacă if
+#define altfel else
+
+#define aliniază_ca alignas
+#define alinierea alignof
+#define mărimea sizeof
+
+#define și &&
+#define _și and
+
+#define automat auto
+
+
+#define și_binar &
+#define sau_binar |
+#define negare_binară ~
+#define sau_exclusiv ^
+#define _și_binar bitand
+#define _sau_binar bitor
+#define _negare_binară compl
+#define _sau_exclusiv xor
+
+
+#define încearca try
+#define prinde catch
+#define implicit default
+
+#define enumerație enum
+
+#define prieten friend
+#define protejat protected
+
+#define mei_la goto
+
+#define in_linie inline
+
+#define registru register
+
+#define reinterpreteaza reinterpret_cast
+
+
+
+
+
+//  Misc
+#define constant const
+
+//  STD stuff
+#define citește std::cin
+#define tipărește std::cout
+#define sfârșit_linie std::endl
+
+//  String library stuff
+#define citește_linia std::cin.getline
+
+//  #trolling
+#define ba_boss {
+#define pa_boss }
+
+#define început {
+#define sfârșit }
+
+#define ho break
+#define mei continue
+
+//  More trolling
+#define principal() main()
+#define jet_acas exit(0)
+
+spațiu ro {
+
+    //  Variabile intregi si reale
+    definește_tip int întreg;
+    definește_tip short s_întreg;
+    definește_tip double real;
+    definește_tip float f_real;
+
+    //  Bool
+    definește_tip bool jurământ;
+
+    //  Caracter
+    definește_tip char caracter;
+
+    //  Void
+    definește_tip void neant;
+
+};
+
